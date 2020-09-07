cb1082c501485eb95a20b8c119ed8554df6c7496
Martin Bertello <martinbertello7@gmail.com>
2020-09-07 18:00:54 -0300

    Login design

--- src/App.vue
+++ src/App.vue
@@ -3,36 +3,17 @@
     <router-view></router-view>
   </v-app>
 </template>
-
 <script>
 export default {
   name: "App"
 };
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '/../css/custom-styles.css';
 
-.v-data-table th {
-    height: 35px !important;
-}
 
-#ghostImage{
-    margin-top: 5%;
-    -webkit-animation: 3s rotate linear infinite;
-    animation: 3s rotate linear infinite;
-    -webkit-transform-origin: 50% 50%;
-    transform-origin: 50% 50%;
+@font-face {
+  font-family: "Login";
+  src: local("login"), url('/../fonts/login.otf') format("truetype");
 }
-@keyframes rotate {from {transform: rotate(0deg);}
-    to {transform: rotate(360deg);}}
-@-webkit-keyframes rotate {from {-webkit-transform: rotate(0deg);}
-  to {-webkit-transform: rotate(360deg);}}
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,38 +1,43 @@
 <template>
   <v-row align="center" justify="center" class="backgroundclass">
     <v-col cols="12">
-      <v-container fluid absolute style="width:25%">
-        <v-card elevation="24">
+      <v-container fluid absolute style="width:20%">
+        <div class="card-login">
           <v-flex>
-          <v-toolbar class="toolbarclass" shaped dark prominent></v-toolbar>
-            <v-form @submit.prevent="onSubmit" novalidate>
+          <div class="toolbarclass"></div>
+            <v-form @submit.prevent="onSubmit" novalidate align="center">
               <v-text-field
-                outlined
+                prepend-inner-icon="mdi-account"
                 :error-messages="errors"
                 type="email"
                 v-model.trim="username"
-                placeholder="Ingrese su nombre de usuario"
+                placeholder="Usuario"
                 dense
+                dark
                 autofocus
-                class="ml-5 mr-5 mt-5"
+                class="ml-5 mr-5 pt-5 pl-5 pr-5 input-login"
                 required
               />
               <v-text-field
-                outlined
                 :error-messages="errors"
+                prepend-inner-icon="mdi-lock"
                 type="password"
                 v-model.trim="password"
-                placeholder="Ingrese su contraseña"
+                placeholder="Contraseña"
                 dense
-                class="ml-5 mr-5"
+                dark
+                class="ml-5 mr-5 pt-3 pl-5 pr-5 input-login"
                 required
               />
-              <v-btn type="submit" dark class="primary ml-5 mb-5 mr-5">INGRESAR</v-btn>
+              <button type="submit" class="button-login mb-5 mt-3">LOGIN</button>
             </v-form>
           </v-flex>
-        </v-card>
+        </div>
       </v-container>
     </v-col>
+    <div>
+      <img src="https://micuenta.donweb.com/img/sectigo_positive_md.png">
+    </div>
   </v-row>
 </template>
 <script>
@@ -78,32 +83,3 @@ export default {
   },
 }
 </script>
-<style>
-  .backgroundclass{
-    background-image: url(https://cdn.vuetifyjs.com/images/parallax/material2.jpg);
-    background-size: 200%;
-    animation: movimiento 20s infinite linear alternate;
-  }
-
-  @keyframes movimiento {
-    from{
-      background-position: bottom left;
-    }to{
-      background-position: top-right;
-    }
-  }
-
-  .toolbarclass{
-    background-image: url(https://cdn.vuetifyjs.com/images/parallax/material2.jpg);
-    background-size: 100%;
-    animation: movimiento 20s infinite linear alternate;
-  }
-
-  @keyframes movimiento2 {
-    from{
-      background-position: bottom left;
-    }to{
-      background-position: top-right;
-    }
-  }
-</style>
