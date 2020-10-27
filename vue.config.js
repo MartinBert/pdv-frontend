dc004db192106bd83dfb3133b8563ea8231f26b6
Martin Bertello <martinbertello7@gmail.com>
2020-10-27 16:15:38 -0300

    Update

--- vue.config.js
+++ vue.config.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 module.exports = {
     devServer: {
       open: process.platform === 'darwin',
+      port: 80,
       https: {
-        key: fs.readFileSync('./prysoftgestion.com.key'),
-        cert: fs.readFileSync('./prysoftgestion.com.crt')
+        key: fs.readFileSync('./cert/cert.crt'),
+        cert: fs.readFileSync('./cert/keyarchive.key')
       },
-      public: 'https://66.97.47.166:80',
+      public: 'https://66.97.47.166',
       disableHostCheck: true,
       hotOnly: false,
     },
