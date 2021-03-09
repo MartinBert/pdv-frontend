c274b6dc15d357218bf1d3e905b45b8f00e2eabe
Martin Bertello <martinbertello7@gmail.com>
2021-03-09 13:56:21 -0300

    Remove inactive components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,4 @@
 <template>
     <v-container class="home-content">
-        <LineChart />
-        <BarChart />
     </v-container>
 </template>
-<script>
-import LineChart from '../components/LineChart';
-import BarChart from '../components/BarChart';
-  export default {
-    components: {
-      LineChart,
-      BarChart
-    },
-  }
-</script>
