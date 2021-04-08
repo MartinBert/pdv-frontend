18d64729d2ec90f9f97a8f8c627c8c86dc783c2a
Ema <egdev5340@gmail.com>
2021-04-08 17:11:25 -0300

    Update

--- src/components/Graphics/LineChartComponent.vue
+++ src/components/Graphics/LineChartComponent.vue
@@ -1,26 +1,71 @@
 <template>
   <div class="small">
-    <BarChart :chartData="charData" />
+    <BarChart :chartData="charData"  v-if="loaded"/>
+    <Spinner v-if="!loaded"/>
   </div>
 </template>
 <script>
 import BarChart from "../../plugins/chart";
+import Spinner from "./Spinner";  
+import GenericService from "../../services/GenericService";
 export default {
   data: () => ({
+    tenant:"",
+    token : localStorage.getItem("token"),
+    loaded : true,
+    filterParams: {
+      sucursalId: "",
+      productoName: "",
+      productoCodigo: "",
+      productoCodigoBarras: "",
+      productoMarcaName: "",
+      productoPrimerAtributoName: "",
+      productoSegundoAtributoName: "",
+      productoTercerAtributoName: "",
+      productoEstado: "",
+      page: 1,
+      size: 10,
+      totalPages: 0,
+    },
+    productos : [], 
     charData: {
-      labels: "Coming Soon",
+      labels:[],
       datasets: [
         {
-          label: "Coming Soon",
+          label :"Productos" , 
           backgroundColor: "rgb(63, 81, 181)",
-          data: [0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+          data: [],
         },
+
       ],
     },
   }),
+
   components: {
     BarChart,
+    Spinner
   },
+
+  mounted(){
+    this.tenant = this.$route.params.tenant
+    this.getData()
+  }, 
+  
+  methods:{
+    getData (){
+      this.loaded = false
+      GenericService(this.tenant,"productos", this.token)
+      .filter(this.filterParams)
+      .then(data=>{
+        this.productos = data.data.content
+        this.productos.forEach(producto => {
+          this.charData.labels.push(producto.nombre)
+          this.charData.datasets[0].data.push(producto.precioTotal)
+        });
+        this.loaded = true 
+      })
+    }
+  }
 };
 </script>
 <style>
