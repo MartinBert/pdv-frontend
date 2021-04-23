6cd882c71163069a7f3efad841b9ff70497e1b70
EGL-dev <egdev5340@gmail.com>
2021-04-23 18:43:56 -0300

    update Calendar

--- src/views/Calendar/Calendar.vue
+++ src/views/Calendar/Calendar.vue
@@ -3,15 +3,32 @@
     <v-col>
       <v-sheet height="64">
         <v-toolbar flat>
-          <v-btn outlined class="mr-4" color="grey darken-2"> Today </v-btn>
+          <v-btn 
+          outlined class="mr-4" 
+          color="grey darken-2"
+          @click="setToday"
+          > Today 
+          </v-btn>
           <v-btn color="primary" dark class="mr-4" @click="dialog = true">
             Agregar
           </v-btn>
-          <v-btn fab text small color="grey darken-2">
+          <v-btn 
+          fab 
+          text small 
+          color="grey darken-2"
+          @click="prev"
+          >
             <v-icon small> mdi-chevron-left </v-icon>
           </v-btn>
-          <v-btn fab text small color="grey darken-2">
-            <v-icon small> mdi-chevron-right </v-icon>
+          <v-btn fab 
+          text 
+          small 
+          color="grey darken-2"
+          @click="next"
+          >
+            <v-icon small>
+               mdi-chevron-right 
+            </v-icon>
           </v-btn>
           <v-toolbar-title v-if="$refs.calendar">
             {{ $refs.calendar.title }}
@@ -47,6 +64,12 @@
           v-model="focus"
           color="primary"
           :type="type"
+          :events="events"
+          :event-color="getEventColor"
+          @click:event="showEvent"
+          @click:more="viewDay"
+          @click:date="viewDay"
+          @change="updateRange"
         ></v-calendar>
 
         <v-dialog v-model="dialog">
@@ -221,6 +244,73 @@ export default {
       this.endEvent = "";
       this.color = "#F0F8FF";
     },
+
+      viewDay ({ date }) {
+        this.focus = date
+        this.type = 'day'
+      },
+      getEventColor (event) {
+        return event.color
+      },
+      setToday () {
+        this.focus = ''
+      },
+      prev () {
+        this.$refs.calendar.prev()
+      },
+      next () {
+        this.$refs.calendar.next()
+      },
+      showEvent ({ nativeEvent, event }) {
+        const open = () => {
+          this.selectedEvent = event
+          this.selectedElement = nativeEvent.target
+          setTimeout(() => {
+            this.selectedOpen = true
+          }, 10)
+        }
+
+        if (this.selectedOpen) {
+          this.selectedOpen = false
+          setTimeout(open, 10)
+        } else {
+          open()
+        }
+
+        nativeEvent.stopPropagation()
+      },
+      updateRange ({ start, end }) {
+        const events = []
+
+        const min = new Date(`${start.date}T00:00:00`)
+        const max = new Date(`${end.date}T23:59:59`)
+        const days = (max.getTime() - min.getTime()) / 86400000
+        const eventCount = this.rnd(days, days + 20)
+
+        for (let i = 0; i < eventCount; i++) {
+          const allDay = this.rnd(0, 3) === 0
+          const firstTimestamp = this.rnd(min.getTime(), max.getTime())
+          const first = new Date(firstTimestamp - (firstTimestamp % 900000))
+          const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
+          const second = new Date(first.getTime() + secondTimestamp)
+
+          events.push({
+            name: this.names[this.rnd(0, this.names.length - 1)],
+            start: first,
+            end: second,
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            timed: !allDay,
+          })
+        }
+
+        this.events = events
+      },
+      rnd (a, b) {
+        return Math.floor((b - a + 1) * Math.random()) + a
+      },
+      
+
+
   },
 };
 </script>
