16ab57bb90929922304b0ffe68941a71d80406da
Ema <egdev5340@gmail.com>
2021-06-15 17:25:56 -0300

    fix tabBar

--- src/components/Graphics/TabBar.vue
+++ src/components/Graphics/TabBar.vue
@@ -1,7 +1,7 @@
 <template>
     <v-container class="d-flex">
         <v-tabs fixed-tabs background-color="primary" dark v-for="(tab, index) in tabs" :key="index">
-            <v-tab :to="tab.route" exact-active-class="black" @click="setActiveTab(tab.id)">{{tab.title}}</v-tab>
+            <v-tab :to="tab.route">{{tab.title}}</v-tab>
         </v-tabs>
     </v-container>
 </template>
