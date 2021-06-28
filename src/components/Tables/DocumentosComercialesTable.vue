2a3c105e8a054910f2262bfe854ab94818bb2880
Ema <egdev5340@gmail.com>
2021-06-28 16:02:13 -0300

    update documentosComerciales/table

--- src/components/Tables/DocumentosComercialesTable.vue
+++ src/components/Tables/DocumentosComercialesTable.vue
@@ -1,19 +1,8 @@
 <template>
   <v-container>
-    <v-simple-table style="background-color: transparent">
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Tipo de documento</th>
-          <th>Letra</th>
-          <th>Acciones</th>
-        </tr>
-      </thead>
-      <tbody v-for="item in items" :key="item.id">
-        <tr>
-          <td>{{ item.nombre }}</td>
-          <td>
-            <v-alert type="success" dense v-if="item.tipo">Fiscal</v-alert>
+    <v-data-table :items="documentosComerciales" :headers="headers">
+      <template v-slot:[`item.tipo`]="{ item }">
+         <v-alert type="success" dense v-if="item.tipo">Fiscal</v-alert>
             <v-alert
               color="secondary"
               icon="mdi-close-circle"
@@ -22,30 +11,67 @@
               v-if="!item.tipo"
               >No fiscal</v-alert
             >
-          </td>
-          <td>{{ item.letra }}</td>
-          <td>
-            <Edit :itemId="item.id" v-on:editItem="editItem" />
-            <Delete :itemId="item.id" v-on:deleteItem="deleteItem" />
-          </td>
-        </tr>
-      </tbody>
-    </v-simple-table>
+      </template>
+      <template v-slot:[`item.acciones`]="{ item }">
+        <v-icon small class="mr-2" @click="editItem(item)">
+          mdi-pencil
+        </v-icon>
+        <v-icon small @click="deleteItem(item)">
+          mdi-delete
+        </v-icon>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 <script>
-import Edit from "../Buttons/Edit";
-import Delete from "../Buttons/Delete";
+//import Edit from "../Buttons/Edit";
+//import Delete from "../Buttons/Delete";
+import GenericService from "../../services/GenericService";
 
 export default {
-  props: {
-    items: Array,
-  },
+  data: () => ({
+    documentosComerciales: [],
+    file: null,
+    filterParams: {
+      documentoComercialName: "",
+      sucursalId: "",
+      page: 1,
+      size: 10,
+      totalPages: 0,
+    },
+    loaded: false,
+    tenant: "",
+    service: "documentosComerciales",
+    token: localStorage.getItem("token"),
+    deleteDialogStatus: false,
+    loguedUser: JSON.parse(localStorage.getItem("userData")),
+    headers: [
+      { text: "Nombre", value: "nombre" },
+      { text: "Tipo de documento", value: "tipo" },
+      { text: "Letra", value: "letra" },
+      { text: "Acciones", value: "acciones", sorteable: false },
+    ],
+  }),
+
   components: {
-    Delete,
-    Edit,
+    // Delete,
+    //Edit,
+  },
+  mounted() {
+    this.tenant = this.$route.params.tenant;
+    this.filterObjects();
   },
   methods: {
+    filterObjects(page) {
+      if (page) this.filterParams.page = page;
+      GenericService(this.tenant, this.service, this.token)
+        .filter(this.filterParams)
+        .then((data) => {
+          this.documentosComerciales = data.data.content;
+          this.filterParams.totalPages = data.data.totalPages;
+          this.loaded = true;
+        });
+    },
     editItem(itemId) {
       this.$emit("editItem", itemId);
     },
--- src/views/PlanesPago/PlanesPago.vue
+++ src/views/PlanesPago/PlanesPago.vue
@@ -1,51 +1,53 @@
 <template>
-  <v-container>
-    <v-form class="mb-3">
-      <v-row>
-        <v-col cols="1">
-          <v-btn class="primary" @click="newObject()" raised>Nuevo</v-btn>
-        </v-col>
-        <v-col cols="3">
-          <v-file-input
-          v-model="file" 
-          class="mt-0"
-          placeholder="Importar planes de pago"
-          accept=".xlsx, xls"
-          @change="importDocuments($event)"
-          ></v-file-input>
-        </v-col>
-        <v-col cols="5"></v-col>
-        <v-col cols="3">
-          <v-text-field
-            v-model="filterParams.planPagoName"
-            v-on:input="filterObjects()"
-            dense
-            outlined
-            rounded
-            placeholder="Búsqueda"
-            append-icon="mdi-magnify"
-          ></v-text-field>
-        </v-col>
-      </v-row>
-    </v-form>
-    <PlanesPagoTable
-      :items="planesPago"
-      v-on:editItem="edit"
-      v-on:deleteItem="deleteItem"
-      v-if="loaded"
-    />
-    <Pagination
-      :page="filterParams.page"
-      :totalPages="filterParams.totalPages"
-      :totalVisible="7"
-      v-on:changePage="filterObjects"
-      v-if="loaded"
-    />
-    <Spinner v-if="!loaded" />
-    <DeleteDialog
-      :status="deleteDialogStatus"
-      v-on:deleteConfirmation="deleteConfirmation"
-    />
+  <v-container style="min-width: 100%;">
+    <v-card>
+      <v-form class="mb-3">
+        <v-row>
+          <v-col cols="1">
+            <v-btn class="primary" @click="newObject()" raised>Nuevo</v-btn>
+          </v-col>
+          <v-col cols="3">
+            <v-file-input
+              v-model="file"
+              class="mt-0"
+              placeholder="Importar planes de pago"
+              accept=".xlsx, xls"
+              @change="importDocuments($event)"
+            ></v-file-input>
+          </v-col>
+          <v-col cols="5"></v-col>
+          <v-col cols="3">
+            <v-text-field
+              v-model="filterParams.planPagoName"
+              v-on:input="filterObjects()"
+              dense
+              outlined
+              rounded
+              placeholder="Búsqueda"
+              append-icon="mdi-magnify"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-form>
+      <PlanesPagoTable
+        :items="planesPago"
+        v-on:editItem="edit"
+        v-on:deleteItem="deleteItem"
+        v-if="loaded"
+      />
+      <Pagination
+        :page="filterParams.page"
+        :totalPages="filterParams.totalPages"
+        :totalVisible="7"
+        v-on:changePage="filterObjects"
+        v-if="loaded"
+      />
+      <Spinner v-if="!loaded" />
+      <DeleteDialog
+        :status="deleteDialogStatus"
+        v-on:deleteConfirmation="deleteConfirmation"
+      />
+    </v-card>
   </v-container>
 </template>
 <script>
@@ -54,7 +56,7 @@ import PlanesPagoTable from "../../components/Tables/PlanesPagoTable";
 import Pagination from "../../components/Pagination";
 import Spinner from "../../components/Graphics/Spinner";
 import DeleteDialog from "../../components/Dialogs/DeleteDialog";
-import XLSX from 'xlsx';
+import XLSX from "xlsx";
 export default {
   data: () => ({
     planesPago: [],
@@ -64,14 +66,14 @@ export default {
       planPagoName: "",
       page: 1,
       size: 10,
-      totalPages: 0
+      totalPages: 0,
     },
     loaded: false,
     tenant: "",
     service: "planesPago",
     token: localStorage.getItem("token"),
     deleteDialogStatus: false,
-    loguedUser: JSON.parse(localStorage.getItem("userData"))
+    loguedUser: JSON.parse(localStorage.getItem("userData")),
   }),
 
   components: {
@@ -83,22 +85,22 @@ export default {
 
   mounted() {
     this.tenant = this.$route.params.tenant;
-    if(this.loguedUser.perfil > 1){
+    if (this.loguedUser.perfil > 1) {
       this.filterParams.sucursalId = this.loguedUser.sucursal.id;
     }
     this.filterObjects();
   },
 
   methods: {
-    filterObjects(page){
-      if(page) this.filterParams.page = page;
+    filterObjects(page) {
+      if (page) this.filterParams.page = page;
       GenericService(this.tenant, this.service, this.token)
         .filter(this.filterParams)
-        .then(data => {
+        .then((data) => {
           this.planesPago = data.data.content;
           this.filterParams.totalPages = data.data.totalPages;
-          if(this.filterParams.totalPages < this.filterParams.page){
-              this.filterParams.page = 1;
+          if (this.filterParams.totalPages < this.filterParams.page) {
+            this.filterParams.page = 1;
           }
           this.loaded = true;
         });
@@ -139,7 +141,7 @@ export default {
       this.file = event;
       var excel = [];
       var reader = new FileReader();
-      reader.onload = e => {
+      reader.onload = (e) => {
         var data = e.target.result;
         var workbook = XLSX.read(data, { type: "binary" });
 
@@ -159,10 +161,10 @@ export default {
             .then(() => {
               this.filterObjects();
               this.loaderStatus = true;
-              window.setTimeout(()=>{
-                this.loader = false
-                this.loaderStatus=false;
-              }, 2000);              
+              window.setTimeout(() => {
+                this.loader = false;
+                this.loaderStatus = false;
+              }, 2000);
             });
         }
       };
@@ -174,19 +176,15 @@ export default {
       var importacion = {
         status: true,
         data: [],
-        message: ""
+        message: "",
       };
       planesPago.forEach((element, index) => {
-        if (
-          element.nombre &&
-          element.cuotas &&
-          element.porcentaje
-        ) {
+        if (element.nombre && element.cuotas && element.porcentaje) {
           var obj = {
             nombre: element.nombre,
             cuotas: element.cuotas,
             porcentaje: element.porcentaje,
-            sucursal: this.loguedUser.sucursal
+            sucursal: this.loguedUser.sucursal,
           };
           importacion.data.push(obj);
         } else {
@@ -195,7 +193,7 @@ export default {
         }
       });
       return importacion;
-    }
-  }
+    },
+  },
 };
 </script>
