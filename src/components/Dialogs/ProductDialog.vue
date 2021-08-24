3ea5e40c29980e802dd22a0239cd423e21f3f616
EGL-dev <egdev5340@gmail.com>
2021-08-24 13:54:55 -0300

    Update

--- src/components/Dialogs/ProductDialog.vue
+++ src/components/Dialogs/ProductDialog.vue
@@ -277,7 +277,7 @@ export default {
     loadProducts() {
       this.$emit("productList", this.$store.state.productos.products);
       this.$store.commit("productos/dialogProductosMutation");
-      this.$refs.table.refresh();
+      this.$refs.pTable.refresh();
     },
 
     clearSelection() {
