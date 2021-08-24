ad1c4064b6325f96f20f070ee8dfdcadc6cd6cc0
EGL-dev <egdev5340@gmail.com>
2021-08-23 21:06:27 -0300

    Update

--- src/views/Cierrez/Cierrez.vue
+++ src/views/Cierrez/Cierrez.vue
@@ -390,7 +390,7 @@ export default {
     },
 
     printZClosure(specification) {
-      ReportsService(this.tenant, "cierres_z", this.token)
+      ReportsService(this.tenant, "ventas", this.token)
         .printZClosure(this.objectToPrint, specification)
         .then((res) => {
           let file = new Blob([res["data"]], {
