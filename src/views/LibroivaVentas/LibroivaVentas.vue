2c61c5a034a87b47ba197b734b6e0ab6e9448a24
EGL-dev <egdev5340@gmail.com>
2021-08-25 15:09:15 -0300

    Update

--- src/views/LibroivaVentas/LibroivaVentas.vue
+++ src/views/LibroivaVentas/LibroivaVentas.vue
@@ -336,7 +336,7 @@ export default {
               el = this.formatForExcel(el);
               dataForExcel.push(el);
             }
-            if (el.nombreDocumento === "Ticket X") {
+            if (el.nombreDocumento === "FACTURAS B") {
               el = this.formatForExcel(el);
               dataForExcel.push(el);
             }
