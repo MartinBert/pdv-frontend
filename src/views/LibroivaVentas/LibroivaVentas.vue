9958999083bfc1159682395a14fd22fe4813f790
EGL-dev <egdev5340@gmail.com>
2021-08-25 14:57:17 -0300

    Update

--- src/views/LibroivaVentas/LibroivaVentas.vue
+++ src/views/LibroivaVentas/LibroivaVentas.vue
@@ -332,7 +332,7 @@ async exportGeneralExcel() {
         .then((data) => {
           let ventas = data.data.content;
           ventas.forEach((el) => {
-            if(this.ventas.nombreDocumento == 'FACTURA'){
+            if(this.ventas.nombreDocumento == 'FACTURAS C'){
                 el = this.formatForExcel(el);
                 dataForExcel.push(el);
                 console.log(ventas);
