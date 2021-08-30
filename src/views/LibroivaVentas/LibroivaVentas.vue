fb6ccbb0ca1709b2cca796ba597adb233ed3d425
Ema <egdev5340@gmail.com>
2021-08-30 14:52:12 -0300

    Update

--- src/views/LibroivaVentas/LibroivaVentas.vue
+++ src/views/LibroivaVentas/LibroivaVentas.vue
@@ -237,7 +237,6 @@ export default {
   },
   mounted() {
     this.tenant = this.$route.params.tenant;
-    this.filterParams.sucursalId = this.loguedUser.sucursal.id
     this.filterObjects();
     this.months = monthsList;
   },
