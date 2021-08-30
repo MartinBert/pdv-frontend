b69ac3a9a4be3fc7fda9cfb29583128896d1e92d
Ema <egdev5340@gmail.com>
2021-08-30 15:06:17 -0300

    Update

--- src/views/Ventas/Ventas.vue
+++ src/views/Ventas/Ventas.vue
@@ -62,9 +62,6 @@ export default {
   },
 
   mounted() {
-      if (this.loguedUser.perfil > 1) {
-      this.filterParams.sucursalId = this.loguedUser.sucursal.id;
-    }
     this.tenant = this.$route.params.tenant;
     this.$store.commit("eventual/resetStates");
   },
