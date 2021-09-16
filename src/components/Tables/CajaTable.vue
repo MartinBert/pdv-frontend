444e90df54431ba76a04debff08b78470161d2ef
Ema <egdev5340@gmail.com>
2021-09-16 16:52:41 -0300

    Update

--- src/components/Tables/CajaTable.vue
+++ src/components/Tables/CajaTable.vue
@@ -25,7 +25,7 @@
     </v-form>
     <v-col cols="3">
       <v-text-field
-        v-model="filterParams.numeroCorrelativo"
+        v-model="filterParams.fecha"
         v-on:input="filterObjects()"
         dense
         outlined
