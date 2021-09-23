6a565e7c16fc29c55297981523b91f5f34d28c09
EGL-dev <egdev5340@gmail.com>
2021-09-23 12:03:56 -0300

    Update

--- src/views/Ventas/VentasForm.vue
+++ src/views/Ventas/VentasForm.vue
@@ -1237,7 +1237,7 @@ export default {
                     comprobante.planesPago = this.object.planesPago;
                     comprobante.nombreDocumento = documento.nombre;
                     comprobante.documentoComercial = documento;
-                    comprobante.totalDescuentos = documento.totalDescuentoGlobal;
+                    comprobante.totalDescuentos = documento;
                   }
 
                   if (this.object.id) {
