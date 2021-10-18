0a1a3af34d00795c82947ec9b17204cdff2701d1
Ema <egdev5340@gmail.com>
2021-10-18 15:37:51 -0300

    update

--- src/views/Impresoras/ImpresorasForm.vue
+++ src/views/Impresoras/ImpresorasForm.vue
@@ -102,6 +102,7 @@ export default {
         .save(this.object)
         .then(() => {
           this.$router.push({ name: "impresoras", params: { id: 0 } });
+          console.log(this.object);
         })
         .catch((error) => {
           if (error.response.status == 500) {
