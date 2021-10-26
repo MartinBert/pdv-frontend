c29289d32b07a248a413e50a04aaed6609a57456
EGL-dev <egdev5340@gmail.com>
2021-10-26 20:22:25 -0300

    add

--- src/views/VentasFast/ventasFast.vue
+++ src/views/VentasFast/ventasFast.vue
@@ -218,7 +218,7 @@ export default {
       document.getElementById(inputId).blur();
     },
 
-    searchWithInput(e,object) {
+    searchWithInput(e,barcode) {
       if (e.keyCode === 13) {
         this.search(this.barCodeSearch);
       }
@@ -228,7 +228,7 @@ export default {
       }
       if (e.keyCode === 69) {
         console.log("eliminando");
-        this.$store.commit("ventasFast/removeProductsToList",object);
+        this.$store.commit("ventasFast/removeProductsToList",barcode);
       }
     },
 
