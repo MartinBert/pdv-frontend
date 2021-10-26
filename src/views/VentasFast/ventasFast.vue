06937d18c75a670c00f09a3710a6244aff414044
Ema <egdev5340@gmail.com>
2021-10-26 15:51:15 -0300

    add

--- src/views/VentasFast/ventasFast.vue
+++ src/views/VentasFast/ventasFast.vue
@@ -224,7 +224,7 @@ export default {
     searchWithInput(e) {
       if (e.keyCode === 13) {
         this.search(this.barCodeSearch);
-      }else{
+      }if(!this.search(this.barCodeSearch)){
         this.barCodeSearch = '';
       }
 
