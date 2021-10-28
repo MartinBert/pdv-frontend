2f64ae9a60def7f6009877a47cd227b4aa37553e
Ema <egdev5340@gmail.com>
2021-10-28 17:49:32 -0300

    add

--- src/components/Tables/ImpresorasTable.vue
+++ src/components/Tables/ImpresorasTable.vue
@@ -31,7 +31,7 @@
           ></v-checkbox>
         </template>
         <template v-slot:[`item.ticket`]="{item,index}">
-          <p v-show="viewCheckboxState === 0">
+          <p v-show="viewCheckboxState === 1">
             {{
               item.ticket
                 ? (checkboxModel[index] = true)
@@ -44,7 +44,7 @@
           ></v-checkbox>
         </template>
         <template v-slot:[`item.etiqueta`]="{item,index}">
-          <p v-show="viewCheckboxState === 0">
+          <p v-show="viewCheckboxState === 1">
             {{
               item.etiqueta
                 ? (checkboxModel[index] = true)
