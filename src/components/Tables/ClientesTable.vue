ea84e08e7cd45978b19f2b49fa2306f2542feee4
EGL-dev <egdev5340@gmail.com>
2021-11-29 18:32:04 -0300

    add

--- src/components/Tables/ClientesTable.vue
+++ src/components/Tables/ClientesTable.vue
@@ -69,6 +69,8 @@ export default {
     clientes: [],
     filterParams: {
       sucursalId: "",
+      nombreRegion:"",
+      nombreEstado:"",
       personaSocialReason: "",
       personaName: "",
       personaCuit: "",
