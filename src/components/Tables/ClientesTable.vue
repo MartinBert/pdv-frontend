991460b7a77b009ea0157ca6db0a5b292301f261
EGL-dev <egdev5340@gmail.com>
2021-11-29 18:46:27 -0300

    add

--- src/components/Tables/ClientesTable.vue
+++ src/components/Tables/ClientesTable.vue
@@ -90,7 +90,7 @@ export default {
       { text: "Nombre", value: "nombre" },
       { text: "Razon Social", value: "razonSocial" },
       { text: "Cuit", value: "cuit" },
-      {text:"Localidad", value:"region", sortablae:false},
+      {text:"Localidad", value:"nombreRegion", sortablae:false},
       { text: "Detalles", value: "detalles", sortable:false},
       { text: "Acciones", value: "acciones" , sortable:false},
     ],
